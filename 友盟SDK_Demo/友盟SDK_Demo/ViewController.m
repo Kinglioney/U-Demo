a7da1479053fbec716ec1894c86d5b80eccc103f
Kinglioney <1402020165@qq.com>
2017-08-13 01:37:29 +0800

    项目初始化

---
+++ 友盟SDK_Demo/友盟SDK_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  友盟SDK_Demo
+//
+//  Created by apple on 2017/8/13.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
